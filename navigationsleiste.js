5b40e4d481c1f3f5454e83d793a6d7c0fdaa175b
Satch2619 <ninho13@hotmail.de>
2022-06-24 22:45:50 +0200

    id für hauptbehaelter erstellt

--- navigationsleiste.js
+++ navigationsleiste.js
@@ -1,4 +1,5 @@
 window.onload=() => {
   const navigationsleiste = document.getElementById("hauptnavogationsleiste");
   const unteresEnde = navigationsleiste.offsetTop + navigationsleiste.offsetHeight;
+  getElementById("hauptbehaelter").style.marginTop = unteresEnde + "px";
 }
