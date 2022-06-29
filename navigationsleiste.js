07160db13250656531475710ebec34c669fb3657
Satch2619 <ninho13@hotmail.de>
2022-06-29 21:16:00 +0200

    JavaScript fertig

--- navigationsleiste.js
+++ navigationsleiste.js
@@ -1,5 +1,10 @@
-window.onload=() => {
-  const navigationsleiste = document.getElementById("hauptnavogationsleiste");
+window.onload =() => {
+  const navigationsleiste = document.getElementbyId("hauptnavigation");
   const unteresEnde = navigationsleiste.offsetTop + navigationsleiste.offsetHeight;
-  getElementById("hauptbehaelter").style.marginTop = unteresEnde + "px";
+  document.getElementById("hauptbehaelter").style.marginTop = unteresEnde + "px";
+}
+
+function navigationUmschalten() {
+  const navigationsleiste = document.getElementById("hauptnavigation");
+  navigationsleiste.classList.toggle("navigation-geöffnet");
 }
